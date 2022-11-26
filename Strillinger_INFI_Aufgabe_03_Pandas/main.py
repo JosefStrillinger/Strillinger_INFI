48d961947da11140b628f02e26d947a15d50905d
Josef Strillinger <jstrillinger@outlook.com>
2022-11-26 17:55:14 +0100

    1.0

--- Strillinger_INFI_Aufgabe_03_Pandas/main.py
+++ Strillinger_INFI_Aufgabe_03_Pandas/main.py
@@ -2,103 +2,156 @@ import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 from tabulate import tabulate
+import seaborn as sns
 
-# Einlesen des Datensatzes
 df = pd.read_excel('data\Zeitreihe-Winter-2022092012.xlsx', skiprows=2)
-
-# Macht man das nicht, heissen die Spalten z.B. 1993 als Zahl. Das kann in Zukunft zu Problemen führen,
-# besser ist sie heissen j1993
-
-
-
 
 base = ['Bezirk','Gemnr','Gemeinde']
 years = df.columns[3:].astype(str)
 base.extend('x' + years)
 df.columns = base
 #print(df.describe())
-#print(df.j1993) # das funktionert jetzt nämlich
-#print(df.Bezirk)
-#print(tabulate(df, headers = df.columns))
-da = df.to_numpy()
 
-print(da)#ist noch 2 dimensional, muss evtl geändert werden
-
-b_i = df[df.Bezirk == 'I']
-c3 = b_i[df.columns[3:]].values[0,:]
-#print(c3)
-v = b_i.values[0,3:]
-#print("V",v)
-plt.plot( v )
-#plt.plot(years, c3)
-plt.xticks(rotation=90)
+# 2.1
+groth_ibk = df.values[2,3:]
+plt.title("Nächtigungen in Ibk: ")
+plt.plot(groth_ibk, "m.")
+plt.xlabel("Year")
+plt.ylabel("Anzahl Nächtigungen")
+plt.savefig("img/2_1.png")
 plt.show()
 
-pd.set_option('display.expand_frame_repr', False) # So werden alle Spalten angezeigt
+# 2.2
+bezirk = df[df.Bezirk == "KU"]
+groth_bezirk = bezirk[df.columns[0:]].values[:,3:].sum(axis=0)
+print(groth_bezirk)
+plt.title("Nachtigungen in Bezirk Kufstein")
+plt.plot(groth_bezirk, "m-")
+plt.xlabel("Jahr")
+plt.ylabel("Anzahl Nächtigungen")
+plt.savefig("img/2_2.png")
+plt.show()
 
-#print(df.describe())
+# 3.1
+min_arr = []
+max_arr = []
+range_arr = []
+avg_arr = []
 
-# Kennenlernen und Überprüfen der Daten
-# print("Die ersten Datensätze:")
-#print(df.head()) # Zeige die ersten Datensätze an
-# print("Die letzten Datensätze:")
-#print(df.tail(3)) # Man kann die Anzahl als Parameter setzen
+for i in range(278):
+    minimum = min(df.values[i,3:])
+    maximum = max(df.values[i,3:])
+    ranges = maximum-minimum
+    average = df.values[i,3:].mean()
+    min_arr.append(minimum)
+    max_arr.append(maximum)
+    range_arr.append(ranges)
+    avg_arr.append(average)
+    
+df["minimum"] = min_arr
+df["maximum"] = max_arr
+df["range"] = range_arr
+df["avg"] = avg_arr
 
-# Auswahl bestimmter Zeilen wie gewohnt
-#print(df[3:])
+# 3.1.1
+standard_range = []
 
-#Zugriff Über den Spaltennamen möglich
-#print(df.Bezirk)
+for i in range(len(range_arr)):
+    standard_range.append(range_arr[i]/avg_arr[i])
+    
+df["standard_range"] = standard_range
 
-# Man kann mehrere Spalten auswählen:
-#print(df[['Gemnr', 'j1993']])
+# 3.2
+tourists_per_year = df[df.columns[3:]].values[1:,:].sum(axis=0)
+print("Touristen pro Jahr: ")
+print(tourists_per_year)
+print("---------------------------------")
+tourists_overall = np.sum(tourists_per_year)
+print("Touristen gesamt: ")
+print(tourists_overall)
+print("---------------------------------")
+all_districts = ["I", "IM", "IL", "KB", "KU", "LA", "LZ", "RE", "SZ"]
+tourists_per_disctricts = []
+for i in range(len(all_districts)):
+    tourists_per_disctricts.append(df[df.Bezirk == all_districts[i]].values[:,3:].sum())
+print(tourists_per_disctricts)
 
-# Über index Location kann man gleich mehrere Spalten auswählen
-#print(df.iloc[:,3:]) # Alle Spalten ab dem Index 3
-#print(df.iloc[:,[3,5,7]]) # Die Spalten 3, 5, 7
+# 4
 
-# Zählen der Bezirke
-#print(df.groupby('Bezirk')['Bezirk'].count())
+#4.1 a
+df.boxplot(column="standard_range", by="Bezirk")
 
-#Aggregieren von (mehreren) Spalten
-#print(df.groupby('Bezirk').agg(sum1993 = ('j1993', 'sum'), sum1994 = ('j1994', 'sum')))
-#print(df.groupby('Bezirk').sum()) # Wendet die Summe auf alle Spalten an
-# Somit können auch Spalten drin sein, die man nicht haben möchte (z.B. Gemnr)
-bez_sum = df.groupby('Bezirk').sum(numeric_only=True)
-bez_sum = bez_sum.loc[:, bez_sum.columns != 'Gemnr']
-#print("Jetzt ohne der Gemeinde:")
-#print(bez_sum)
-# Aufsummieren von Spalten
-nr_cols = df.iloc[:,3:]
-#print(nr_cols.sum())
+#4.1 b
+pos = 0
+labels = df["Bezirk"].unique()
+for b in labels:
+    bez = df[df.Bezirk == b]
+    plt.boxplot(bez["standard_range"], positions=[pos])
+    pos += 1
+    plt.xticks(range(len(labels)), labels)
+    
+#4.1 c
+sns.boxenplot(x = df["Bezirk"], y = df["standard_range"], data=df)
+plt.savefig("img/4_2.png")
+plt.show()
 
+# 4.2
+sns.barplot(x = df.columns[3:26], y = df.values[2,3:26], palette="terrain")
+plt.show()
 
-#bez_sum_1993_1994 = df.loc[:, ['Bezirk', 'j1993', 'j1994']].groupby('Bezirk').sum()
+# 5
+df_2 = pd.read_excel("data/bev_meld.xlsx")
 
+data = pd.merge(df, df_2, how="inner", on="Gemnr")
+data = data.drop(columns="Gemnr")
 
-#print(bez_sum_1993_1994)
-# Aufsummieren von Zeilen
-#row_sum = nr_cols.sum(axis=1)
-#df['sum_all'] = row_sum
+base = ["Bezirk", "Gemeinde"]
+years = data.columns[2:25].astype(str)
+base_2 = ["minimum", "maximum", "range", "avg", "standard_range", "Bezirk2", "Gemeinde2"]
+years_2 = data.columns[32:].astype(str)
+base.extend(years)
+base.extend(base_2)
+base.extend("y" + years_2)
+data.columns = base
 
-# print(df.head())
+# 5 a
+stays_per_person_2018 = data["x2018"]/data["y2018"]
+stays_per_person_2020 = data["x2020"]/data["y2020"]
 
-# Auswahl einzelner Zeilen
-#df_il = df.loc[df.Bezirk == 'IL'] # Nur Innsbruck - Land
-#print(df_il)
+# 5 b
+sns.boxplot(x = data["Bezirk"], y = stays_per_person_2018, data=data)
+plt.savefig("img/5_b_1.png")
+plt.show()
 
-# Erste Plots
-#Die Entwicklung der tiroler Gesamtbevölkerung über die Jahre
-#plt.plot(nr_cols.sum())
-#plt.xticks(rotation=90)
-#plt.show()
+sns.boxplot(x = data["Bezirk"], y = stays_per_person_2020, data=data)
+plt.savefig("img/5_b_2.png")
+plt.show()
 
-#df = df.drop(columns='Gemnr') # So kann man eine Spalte (oder mehrere wegschmeissen)
+# 5 c
+lowest_ten = stays_per_person_2020.sort_values(ascending=True)
+highest_ten = stays_per_person_2020.sort_values(ascending=False)
 
-# Sortieren nach einer (oder mehreren) Spalten
-#df_high = df.sort_values('j1993', ascending=False)
-#print(df_high.head())
+lowest_indexes = []
+highest_indexes = []
+bezirke = data["Gemeinde"]
 
-# Alle die in Innsbruck Land und Imst sind
-#il_im = df.loc[df['Bezirk'].isin(['IL','IM'])]
-#print(il_im)
+for i in range(0,10):
+    lowest_indexes.append(bezirke[lowest_ten.index[stays_per_person_2020 == lowest_ten[i]]].values[0])
+    highest_indexes.append(bezirke[highest_ten.index[stays_per_person_2020 == highest_ten[i]]].values[0])
+    
+sns.boxplot(x = lowest_indexes, y = lowest_ten[:10], data=data)
+plt.savefig("img/5_c_1.png")
+plt.show()
+
+sns.boxplot(x = highest_indexes, y = highest_ten[:10], data=data)
+plt.savefig("img/5_c_2.png")
+plt.show()
+
+# 5 d
+plt.title("Nächtigungen pro Kopf in Angerberg")
+plt.plot(stays_per_person_2020[139-5], "m.")# Angerberg in Excel: Zeile 139 
+print(stays_per_person_2020[139-5])
+plt.xlabel("Gemeinde")
+plt.ylabel("Nächtigungen pro Kopf")
+plt.savefig("img/5_d.png")
+plt.show()
